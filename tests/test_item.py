482e6efb3f50391d80416d66b44e1cd477b3f56a
Artur Khamidulliun <artrimskiy@gmail.com>
2024-01-24 22:32:52 +0500

    write tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_item():
+    item = Item("Смартфон", 10000, 20)
+    assert item.calculate_total_price() == 200000
+
+    Item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000.0
+
