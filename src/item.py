ae6929c1f473d952dd960a3ae98621a4ef001842
Artur Khamidulliun <artrimskiy@gmail.com>
2024-01-31 04:17:23 +0500

    write code for homework 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +34,29 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, cvs_file):
+        with open(cvs_file, newline='', encoding='windows-1251') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cls(str(row["name"]), float(row["price"]), int(row["quantity"]))
+
+    @staticmethod
+    def string_to_number(str_number):
+        """Возвращает число из числа-строки"""
+        number = float(str_number)
+        return int(number)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            self.__name = new_name[:10]
+        else:
+            self.__name = new_name
+        #   raise Exception('Длина наименования товара превышает 10 символов.')
+
