0ba94b8a1d83dd53fb47e23016150f2f88014c5d
Artur Khamidulliun <artrimskiy@gmail.com>
2024-02-07 00:49:56 +0500

    create and write test for homework 4

---
+++ tests/test_phone.py
@@ -0,0 +1,29 @@
+import pytest
+from src.phone import Phone
+
+
+@pytest.fixture
+def phone():
+    return Phone("iPhone 14", 120_000, 5, 2)
+
+
+def test_phone(phone):
+    assert phone.name == "iPhone 14"
+    assert phone.price == 120_000
+    assert phone.quantity == 5
+    assert phone.number_of_sim == 2
+
+
+def test_repr(phone):
+    assert repr(phone) == "Phone('iPhone 14', 120000, 5, 2)"
+
+
+def test_number_of_sim(phone):
+    with pytest.raises(ValueError):
+        phone.number_of_sim = 0
+        phone.number_of_sim = -1
+        phone.number_of_sim = 1.2
+
+
+def test_str(phone):
+    assert str(phone) == 'iPhone 14'
