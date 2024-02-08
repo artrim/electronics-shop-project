0f5ab90c6ad200c17ca6defae8f66e7b6a231940
Artur Khamidulliun <artrimskiy@gmail.com>
2024-02-09 03:52:54 +0500

    create change_lang.py

---
+++ src/change_lang.py
@@ -0,0 +1,14 @@
+class ChangeLang:
+
+    def __init__(self):
+        self.__language = 'EN'
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        else:
+            self.__language = 'EN'
